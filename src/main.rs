93b217c9085c4ceff5a24296650162c3b4b24751
Elkeid <elkeid.me@gmail.com>
2023-07-23 12:58:21 +0800

    fix arg nums

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ mod document_parser;
 
 fn main() -> Result<()> {
     let args = env::args().collect::<Vec<_>>();
-    if args.len() != 2 {
+    if args.len() != 3 {
         println!("参数数目错误.");
         process::exit(1);
     }
     let config = config_parser::parser_config("clomeaste_config.clcfg");
-    let unparsed_tml = fs::read_to_string(&args[0])?;
+    let unparsed_tml = fs::read_to_string(&args[1])?;
     let document = document_parser::parse_tml(&unparsed_tml).expect("解析文档时出现错误.");
-    let mut file = fs::File::create(&args[1])?;
+    let mut file = fs::File::create(&args[2])?;
     document.dump(&mut file, &config);
     Ok(())
 }
